6be962b920c085cd831f7ee2d67dd4d80b3e02ab
Ramiroqr <ramiroquisper@gmail.com>
2023-10-07 08:53:18 -0500

    Create header section

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,16 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        {/* Header */}
+          <Header />
+        {/* NavBar */}
+
+        {/* SearchBox */}
         {children}
       </body>
     </html>
