6b8fa3fa0e75f7c788e00b4640e1275caaae3a1e
Ramiroqr <ramiroquisper@gmail.com>
2023-10-09 08:33:25 -0500

    About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,23 @@
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-500">About</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum massa sapien. Nunc at ornare justo. Proin aliquet eleifend ante, sit amet dignissim ex cursus luctus. Maecenas et tristique enim. Donec consequat dignissim ligula, ut egestas tellus ornare vel. Suspendisse tincidunt condimentum lectus at scelerisque. Curabitur luctus lectus eu felis varius, nec pellentesque mauris ultricies. Sed vehicula egestas augue, id ultrices lectus mollis eu.
+      </p> 
+      
+      <p>
+        Sed mollis ante odio, id vehicula libero convallis vitae. Curabitur quis mi consequat, ultricies purus et, lobortis erat. Aliquam pretium luctus lacus, eu finibus elit euismod eget. Mauris quis pharetra nunc, a suscipit mi. Etiam placerat mi nisi, auctor tristique tortor rhoncus a. Vestibulum ac enim tempor, tristique sapien non, tempus libero.
+      </p>
+
+      <p>
+        In fringilla, nisl sit amet ultrices maximus, lectus eros placerat mauris, interdum condimentum nisi dolor vel ipsum. Mauris vel sodales lacus. Duis tincidunt ligula et semper dapibus. Aenean non aliquet eros. Fusce aliquet pulvinar leo quis pellentesque. Vestibulum sodales sapien sagittis ante venenatis lobortis. 
+        
+        Donec sed consectetur massa. Sed sed luctus massa. Nullam a fermentum nulla, nec elementum quam. Phasellus congue erat malesuada vulputate ornare. Donec ac fermentum orci. Morbi aliquam hendrerit nisi sit amet sodales. In quis est eu lectus luctus aliquam. Proin sodales eget dui vitae facilisis. Nunc et molestie quam, nec consequat enim
+      </p>
+      
+    </div>
+  )
+}
